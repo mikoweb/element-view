d950bfa0e1baa46584008f625e65eca2d57494c8
Rafał Mikołajun <root@rmweb.pl>
2017-12-19 21:11:18 +0100

    mixin test

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'bundle/element-view.js',
-      'test/**/**.js'
+      'tests/**/**.js'
     ],
 
 
